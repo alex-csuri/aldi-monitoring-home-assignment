0f07aef16b1fd8b91c1e0c578475df2afb7eae1f
alex-csuri <4lexkingfisher@gmail.com>
2025-10-16 12:31:00 +0200

    Task 10 - implemented getMovingAverage method to calculate moving averages based on given window sizes

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -1,6 +1,7 @@
 package com.aldisued.iot.monitoring.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -33,8 +34,25 @@ public class MeasurementCalculatorService {
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
-    // TODO: Task 10
-    return List.of();
+      if (data == null || data.isEmpty()) {
+          throw new IllegalArgumentException("No data available to parse.");
+      }
+      if (windowSize <= 0 || windowSize > data.size()) {
+          throw new IllegalArgumentException("Invalid window size.");
+      }
+
+      List<Double> movingAverages = new ArrayList<>();
+
+      // calculate moving average based on given window size
+      for (int i = 0; i <= data.size() - windowSize; i++) {
+          double sum = 0.0;
+          for (int j = i; j < i + windowSize; j++) {
+              sum += data.get(j);
+          }
+          movingAverages.add(sum / windowSize);
+      }
+
+      return movingAverages;
   }
 
 }
