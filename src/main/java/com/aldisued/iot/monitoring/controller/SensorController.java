231491524979d4e64a96761421943978b465f404
alex-csuri <4lexkingfisher@gmail.com>
2025-10-16 10:03:59 +0200

    Task 4 - fixed exception handling for HTTP requests when creating Sensor entities

--- src/main/java/com/aldisued/iot/monitoring/controller/SensorController.java
+++ src/main/java/com/aldisued/iot/monitoring/controller/SensorController.java
@@ -3,6 +3,8 @@ package com.aldisued.iot.monitoring.controller;
 import com.aldisued.iot.monitoring.dto.SensorDto;
 import com.aldisued.iot.monitoring.entity.Sensor;
 import com.aldisued.iot.monitoring.service.SensorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,14 @@ public class SensorController {
   }
 
   @PostMapping
-  public Sensor saveSensor(@RequestBody SensorDto sensorDto) {
-    return sensorService.saveSensor(sensorDto);
+  public ResponseEntity<?> saveSensor(@RequestBody SensorDto sensorDto) {
+      try {
+          Sensor sensorToSave = sensorService.saveSensor(sensorDto);
+          return ResponseEntity.ok(sensorToSave);
+      } catch (IllegalArgumentException e) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+      } catch (IllegalStateException e) {
+          return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+      }
   }
 }
--- src/main/java/com/aldisued/iot/monitoring/service/SensorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/SensorService.java
@@ -5,6 +5,8 @@ import com.aldisued.iot.monitoring.entity.Sensor;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SensorService {
 
@@ -15,6 +17,13 @@ public class SensorService {
   }
 
   public Sensor saveSensor(SensorDto sensor) {
+    if (sensor.name() == null || sensor.name().isBlank()) {
+        throw new IllegalArgumentException("Sensor name must not be null or empty.");
+    }
+    List<Sensor> sensors = sensorRepository.findAll();
+    if (sensors.stream().anyMatch(s -> s.getName().equalsIgnoreCase(sensor.name()))) {
+        throw new IllegalStateException("A sensor with that name already exists.");
+    }
     return sensorRepository.save(new Sensor(
         sensor.name(),
         sensor.type()
